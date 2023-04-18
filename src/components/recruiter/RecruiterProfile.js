1df6ac5e645b86cb44a969d2aca6796003b47e04
jeff ndungu <ndungujeff.ke@gmail.com>
2023-04-18 17:16:07 +0300

    working on the landing page

--- src/components/recruiter/RecruiterProfile.js
+++ src/components/recruiter/RecruiterProfile.js
@@ -3,9 +3,9 @@ import React from 'react'
 const RecruiterProfile = () => {
   return (
     <div>
-    <div class="w-full max-w-sm p-3 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+    <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
         <form class="space-y-3" action="#">
-            <h5 class="text-xl font-medium text-gray-900 dark:text-white">Moringa  BootCamp</h5>
+            <h5 class="text-xl font-medium text-gray-900 dark:text-white">Moringa Tech BootCamp</h5>
             <div>
                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                 <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required/>
