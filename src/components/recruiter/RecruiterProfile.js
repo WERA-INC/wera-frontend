4043a37a62b280d992374801f84f12f9792aa564
jeff ndungu <ndungujeff.ke@gmail.com>
2023-04-19 22:37:52 +0300

    working on the landing page,navbar

--- src/components/recruiter/RecruiterProfile.js
+++ src/components/recruiter/RecruiterProfile.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React from "react";
 
 const RecruiterProfile = () => {
   return (
-    <div>
-    </div>
+  //   <div class="row">
+  //   <div class="col-md-4 offset-md-9 ">
+  //     <div class="card" style={{"width": 250}}>
+  //       <div class="card-body">
+  //         <h5 class="card-title">Card title</h5>
+  //         <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+  //         <p class="card-text">
+  //           Some quick example text to build on the card title and make up
+  //           the bulk of the card's content.
+  //         </p>
+  //         <a href="#" class="card-link">
+  //           Card link
+  //         </a>
+  //         <a href="#" class="card-link">
+  //           Another link
+  //         </a>
+  //       </div>
+  //     </div>
+  //   </div>
+  // </div>
+  <div> 
+<div class="max-w-sm mx-auto my-8 rounded overflow-hidden shadow-lg bg-gray-100">
+<div class="px-6 py-4">
+<div class="font-bold text-xl mb-2">Moringa Tech BootCamp</div>
+</div>
+<div class="px-6 pt-4 pb-2">
+<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">moringa@tech.com</span>
+<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Ngong Lane, Ngong Lane Plaza, 1st Floor, Nairobi Kenya</span>
+<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Type;full time, part time</span>
+<span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">https://moringaschool.com/</span>
+</div>
+</div>
+</div>
+  );
+};
 
-  )
-}
-
-export default RecruiterProfile
-
+export default RecruiterProfile;
