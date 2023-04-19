7aeb71c0e92865176a0bda1551b45d0888495cbb
Marcus <marcus.kariuki@student.moringaschool.com>
2023-04-19 16:46:05 +0300

    feat:add dropdown to Navbar add header background

--- src/components/LandingHomePage/Navbar.js
+++ src/components/LandingHomePage/Navbar.js
@@ -1,88 +1,138 @@
 import React, { useState } from "react";
-import { Menu, Button, Dropdown } from "semantic-ui-react";
 import { Link } from "react-scroll";
-import './LandingHomePage.css'
-import "semantic-ui-css/semantic.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "./LandingHomePage.css";
+import "./Navbar.css";
 
 const Navbar = () => {
-  const [menuVisible, setMenuVisible] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleScrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
-
-  const handleMenuToggle = () => {
-    setMenuVisible(!menuVisible);
-  };
-
   return (
-    <div className="home-navbar">
-      <Menu className="menu">
-        <Menu.Item header className="home-logo">WERA</Menu.Item>
-
-        <Menu.Menu position="right">
-          <Menu.Item>
-            <Button
-              color="blue"
-              icon="bars"
-              onClick={handleMenuToggle}
-              className="mobile-menu-icon"
-            />
-          </Menu.Item>
-        </Menu.Menu>
-
-        {menuVisible && (
-          <Dropdown item text="Menu" className="mobile-menu">
-            <Dropdown.Menu>
-              <Dropdown.Item>
-                <Link
-                  to="aboutSection"
-                  smooth={true}
-                  duration={200}
-                  spy={true}
-                  exact="true"
-                  onClick={handleMenuToggle}
+    <>
+      <div>
+        <div class="header-blue">
+          <nav class="navbar navbar-light navbar-expand-md navigation-clean-search">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">
+                BBBOOTSTAP
+              </a>
+              <button
+                data-toggle="collapse"
+                class="navbar-toggler"
+                data-target="#navcol-1"
+              >
+                <span class="sr-only">Toggle navigation</span>
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav">
+                  <li class="nav-item" role="presentation">
+                    <a class="nav-link" href="#">
+                      Contact
+                    </a>
+                  </li>
+               
+                  {/* <li class="nav-item dropdown">
+                    <a
+                      class="dropdown-toggle nav-link"
+                      data-toggle="dropdown"
+                      aria-expanded="false"
+                      href="#"
+                    >
+                      Services
+                    </a>
+                    <div class="dropdown-menu" role="menu">
+                      <a class="dropdown-item" role="presentation" href="#">
+                        Logo design
+                      </a>
+                      <a class="dropdown-item" role="presentation" href="#">
+                        Banner design
+                      </a>
+                      <a class="dropdown-item" role="presentation" href="#">
+                        content writing
+                      </a>
+                    </div>
+                  </li> */}
+                </ul>
+                <form class="form-inline mr-auto" target="_self">
+                  <div class="form-group">
+                    <label for="search-field">
+                      <i class="fa fa-search text-white"></i>
+                    </label>
+                    <input
+                      class="form-control search-field"
+                      type="search"
+                      id="search-field"
+                      name="search"
+                    />
+                  </div>
+                </form>
+                <span class="navbar-text">
+                  {" "}
+                  <a class="login" href="#">
+                    Log In
+                  </a>
+                </span>
+                <div class="dropdown">
+                    <button
+                      type="button"
+                      class="btn btn-primary dropdown-toggle"
+                      data-bs-toggle="dropdown"
+                    >
+                      Sign Up
+                    </button>
+                    <ul class="dropdown-menu">
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Dashboard
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Employer
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Job Seeker
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+              </div>
+            </div>
+          </nav>
+          <div class="container hero">
+            <div class="row">
+              <div class="col-12 col-lg-6 col-xl-5 offset-xl-1">
+                <h1>Business goal designs</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.<br></br>
+                </p>
+                <button
+                  class="btn btn-light btn-lg action-button"
+                  type="button"
                 >
-                  About Us
-                </Link>
-              </Dropdown.Item>
-              <Dropdown.Item>
-                <Link
-                  to="teamSection"
-                  smooth={true}
-                  duration={150}
-                  spy={true}
-                  exact="true"
-                  onClick={handleMenuToggle}
-                >
-                  Team
-                </Link>
-              </Dropdown.Item>
-              <Dropdown.Item>
-                <Link
-                  to="feature"
-                  smooth={true}
-                  duration={200}
-                  spy={true}
-                  exact="true"
-                  onClick={handleMenuToggle}
-                >
-                  Feature
-                </Link>
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        )}
-      </Menu>
-
-      <Button
-        color="blue"
-        icon="arrow up"
-        onClick={handleScrollToTop}
-        style={{ position: "fixed", bottom: "20px", right: "20px" }}
-      />
-    </div>
+                  Learn More<i class="fa fa-long-arrow-right ml-2"></i>
+                </button>
+              </div>
+              <div class="col-md-5 col-lg-5 offset-lg-1 offset-xl-0 d-none d-lg-block phone-holder">
+                <div class="iphone-mockup">
+                  <img class="device" src="https://i.imgur.com/bkCeTu7.png" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default Navbar;
