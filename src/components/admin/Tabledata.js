11d6e3dbd2f0e86914d3268ac43c4105df4c521f
Elizabeth <mwendemuthusi6@gmail.com>
2023-04-20 16:50:06 +0300

    Modified styling to the search bar, button and table header

--- src/components/admin/Tabledata.js
+++ src/components/admin/Tabledata.js
@@ -2,17 +2,25 @@ import React from "react";
 
 const Tabledata = () => {
   return (
-    <tr>
-      <th>
-        <label class="customcheckbox m-b-20">
-          <input type="checkbox" id="mainCheckbox" />
-          <span class="checkmark"></span>
-        </label>
-      </th>
+    <tr className="p"> 
       <td>12/11/2022</td>
       <td>Bill</td>
       <td>Clinton</td>
       <td>ICT</td>
+      <th>
+        <div class="checkbox">
+          <label>
+            <input
+              type="checkbox"
+              checked
+              data-toggle="toggle"
+              data-style="ios"
+              data-onstyle="info"
+              data-offstyle="danger"
+            />
+          </label>
+        </div>
+      </th>
     </tr>
   );
 };
