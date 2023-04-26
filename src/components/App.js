1d77dafb3538c46bbab02440af35830fcae71340
Jose <jose.muhlanga@student.moringaschool.com>
2023-04-26 10:12:20 +0300

    Modification of App.js

--- src/components/App.js
+++ src/components/App.js
@@ -28,7 +28,7 @@ import RecruiterJobsTable from "./company/tables/RecruiterJobsTable.js";
 import JobDetails from "./company/views/JobDetails.js";
 
 function App() {
-  const [jobseeker, setJobseeker] = useState(null);
+  const [jobseeker, setJobseeker]=useState(null)
   // useEffect(() => {
   //   const jsId = localStorage.getItem("jobseekerId");
   //   console.log(jsId);
