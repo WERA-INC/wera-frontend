4b96e3803087cdb374d68dbc399734405781b428
Marcus <marcus.kariuki@student.moringaschool.com>
2023-05-01 14:57:42 +0300

    feat:add industry tags in the profile component

--- src/components/jobseeker/profilecomponents/education.js
+++ src/components/jobseeker/profilecomponents/education.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Education = () => {
-    const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-  
-    const toggleEdit = () => {
-      setIsEditModalOpen(!isEditModalOpen);
-    };
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+
+  const toggleEdit = () => {
+    setIsEditModalOpen(!isEditModalOpen);
+  };
   // const [isEditing, setIsEditing] = useState(false);
 
   // const toggleEdit = () => {
@@ -199,10 +199,7 @@ const Education = () => {
             {educationData.map((education) => (
               <div key={education.id} className=" rounded p-4 mb-4">
                 <div className="text-white">
-                  <section
-                    className="w-full sm:w-64 mx-auto bg-[#0a1f38] rounded-2xl px-8 py-6 shadow-lg h-auto sm:h-100">
-
-
+                  <section className="w-full sm:w-64 mx-auto bg-[#0a1f38] rounded-2xl px-8 py-6 shadow-lg h-auto sm:h-100">
                     <div className="flex items-center justify-between">
                       <span className="text-emerald-400">
                         <svg
@@ -228,28 +225,19 @@ const Education = () => {
                       <p>Qualification: {education.qualification}</p>
                     </div>
                     <div className="flex justify-center gap-1 p-2 mt-4">
-                    <div className="mt-3 text-white text-sm">
-                      <button
-                        type="button"
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                        onClick={toggleEdit}
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-5 w-5 mr-2"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
+                      <div className="mt-3 text-white text-sm">
+                        <button
+                          className="inline-flex items-center px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white text-sm font-medium rounded-md"
+                          onClick={toggleEdit}
                         >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            srokeWidth="2"
-                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                          />
-                        </svg>
-                        Delete
-                      </button>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 512 512"
+                          >
+                            <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
+                          </svg>
+                          Edit
+                        </button>
                       </div>
 
                       <div className="mt-3 text-white text-sm">
@@ -278,80 +266,86 @@ const Education = () => {
                   </section>
                 </div>
                 {isEditModalOpen && (
-                   <div className="fixed z-10 inset-0 overflow-y-auto">
-                   <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-                     <div className="fixed inset-0 transition-opacity" aria-hidden="true">
-                       <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
-                     </div>
-         
-                     <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
-                       &#8203;
-                     </span>
-         
-                     <div
-                       className="inline-block align-bottom bg-[#0D2644] rounded-lg px-4 pt-2 pb-4 text-center overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6"
-                       role="dialog"
-                       aria-modal="true"
-                       aria-labelledby="modal-headline"
-                     >
-                       {/* Edit form content */}
-                     
-                    <form className=" rounded p-3 mt-4">
-                      <input
-                        type="text"
-                        name="year_of_admission"
-                        placeholder="Year of Admission"
-                        value={formData.year_of_admission}
-                        onChange={handleChange}
-                        className="bg-dark placeholder-white rounded p-2 mb-2"
-                      />
-                      <input
-                        type="text"
-                        name="year_of_completion"
-                        placeholder="Year of Completion"
-                        value={formData.year_of_completion}
-                        onChange={handleChange}
-                        className="bg-dark placeholder-white  rounded p-2 mb-2"
-                      />
-                      <input
-                        type="text"
-                        name="institution"
-                        placeholder="Institution"
-                        value={formData.institution}
-                        onChange={handleChange}
-                        className="bg-dark placeholder-white  rounded p-2 mb-2"
-                      />
-                      <input
-                        type="text"
-                        name="qualification"
-                        placeholder="Qualification"
-                        value={formData.qualification}
-                        onChange={handleChange}
-                        className="bg-dark placeholder-white  rounded p-2 mb-2"
-                      />
-                     
-                    
-                    </form>
-                    <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
-                        onClick={() =>
-                          handleUpdateEducation(educationData.id, educationData)
-                        }
+                  <div className="fixed z-10 inset-0 overflow-y-auto">
+                    <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                      <div
+                        className="fixed inset-0 transition-opacity"
+                        aria-hidden="true"
                       >
-                        Update
-                      </button>
-                  
+                        <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+                      </div>
+
+                      <span
+                        className="hidden sm:inline-block sm:align-middle sm:h-screen"
+                        aria-hidden="true"
+                      >
+                        &#8203;
+                      </span>
+
+                      <div
+                        className="inline-block align-bottom bg-[#0D2644] rounded-lg px-4 pt-2 pb-4 text-center overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6"
+                        role="dialog"
+                        aria-modal="true"
+                        aria-labelledby="modal-headline"
+                      >
+                        {/* Edit form content */}
+
+                        <form className=" rounded p-3 mt-4">
+                          <input
+                            type="text"
+                            name="year_of_admission"
+                            placeholder="Year of Admission"
+                            value={formData.year_of_admission}
+                            onChange={handleChange}
+                            className="bg-dark placeholder-white rounded p-2 mb-2"
+                          />
+                          <input
+                            type="text"
+                            name="year_of_completion"
+                            placeholder="Year of Completion"
+                            value={formData.year_of_completion}
+                            onChange={handleChange}
+                            className="bg-dark placeholder-white  rounded p-2 mb-2"
+                          />
+                          <input
+                            type="text"
+                            name="institution"
+                            placeholder="Institution"
+                            value={formData.institution}
+                            onChange={handleChange}
+                            className="bg-dark placeholder-white  rounded p-2 mb-2"
+                          />
+                          <input
+                            type="text"
+                            name="qualification"
+                            placeholder="Qualification"
+                            value={formData.qualification}
+                            onChange={handleChange}
+                            className="bg-dark placeholder-white  rounded p-2 mb-2"
+                          />
+                        </form>
                         <button
-                        type="button"
-                        className=" bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-2"
-                        onClick={toggleEdit}
-                      >
-                        Cancel
-                      </button>
-                     </div>
-                   </div>
-                 </div>
-                
+                          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
+                          onClick={() =>
+                            handleUpdateEducation(
+                              educationData.id,
+                              educationData
+                            )
+                          }
+                        >
+                          Update
+                        </button>
+
+                        <button
+                          type="button"
+                          className=" bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-2"
+                          onClick={toggleEdit}
+                        >
+                          Cancel
+                        </button>
+                      </div>
+                    </div>
+                  </div>
                 )}
               </div>
             ))}
